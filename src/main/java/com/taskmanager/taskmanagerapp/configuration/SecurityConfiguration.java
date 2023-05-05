a199fcf10307140802d36e04ca03f3f5175f5046
MAZJ124 <yuangazi@hotmail.com>
2023-05-06 05:52:09 +0800

    Add basic spring security authorization

--- src/main/java/com/taskmanager/taskmanagerapp/configuration/SecurityConfiguration.java
+++ src/main/java/com/taskmanager/taskmanagerapp/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.taskmanager.taskmanagerapp.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,11 +15,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user1")
-                .password("password1")
+                .password("password")
                 .roles("USER")
                 .and()
                 .withUser("admin1")
-                .password("password1")
+                .password("password")
                 .roles("ADMIN");
     }
 
@@ -26,4 +27,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public PasswordEncoder getEncodedPassword() {
         return NoOpPasswordEncoder.getInstance();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/task-manager/tasks/**").hasAnyRole("ADMIN", "USER")
+                .and().formLogin();
+    }
+
+
 }
