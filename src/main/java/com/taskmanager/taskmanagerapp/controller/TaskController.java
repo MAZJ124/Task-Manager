fe68e78be5acd0bb7a4992d46b6795b56f1adef3
MAZJ124 <yuangazi@hotmail.com>
2023-05-06 04:30:52 +0800

    Add exception handling for some APIs

--- src/main/java/com/taskmanager/taskmanagerapp/controller/TaskController.java
+++ src/main/java/com/taskmanager/taskmanagerapp/controller/TaskController.java
@@ -1,12 +1,12 @@
 package com.taskmanager.taskmanagerapp.controller;
 
 import com.taskmanager.taskmanagerapp.model.Task;
+import com.taskmanager.taskmanagerapp.response.ResponseHandler;
 import com.taskmanager.taskmanagerapp.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/task-manager")
@@ -16,31 +16,42 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping("/tasks")
-    public ResponseEntity<List<Task>> getAllTasks() {
-        return ResponseEntity.ok(taskService.getAllTasks());
+    public ResponseEntity<Object> getAllTasks() {
+        return ResponseHandler.generateResponse("success", HttpStatus.OK, taskService.getAllTasks());
     }
 
     @PostMapping("/tasks")
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        return ResponseEntity.ok(taskService.createTask(task));
+    public ResponseEntity<Object> createTask(@RequestBody Task task) {
+        return ResponseHandler.generateResponse("success", HttpStatus.OK, taskService.createTask(task));
     }
 
     @GetMapping("/tasks/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        return ResponseEntity.ok(taskService.getTaskById(id));
+    public ResponseEntity<Object> getTaskById(@PathVariable Long id) {
+        try {
+            return ResponseHandler.generateResponse("success", HttpStatus.OK, taskService.getTaskById(id));
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.toString(), HttpStatus.BAD_GATEWAY, null);
+        }
     }
 
     @PutMapping("/tasks/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
-        task.setId(id);
-        return ResponseEntity.ok(taskService.updateTask(task));
+    public ResponseEntity<Object> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        try {
+            task.setId(id);
+            return ResponseHandler.generateResponse("success", HttpStatus.OK, taskService.updateTask(task));
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.toString(), HttpStatus.BAD_GATEWAY, null);
+        }
     }
 
     @DeleteMapping("/tasks/{id}")
-    public ResponseEntity<Boolean> deleteTask(@PathVariable Long id) {
-        taskService.deleteTask(id);
-        return ResponseEntity.ok(true);
+    public ResponseEntity<Object> deleteTask(@PathVariable Long id) {
+        try {
+            taskService.deleteTask(id);
+            return ResponseHandler.generateResponse("success", HttpStatus.OK, "Task deleted successfully");
+        } catch (Exception e) {
+            return ResponseHandler.generateResponse(e.toString(), HttpStatus.BAD_GATEWAY, null);
+        }
     }
-
 
 }
