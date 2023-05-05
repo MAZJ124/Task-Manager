9de767933a778b06b31042d9a1714b5e95a04263
MAZJ124 <yuangazi@hotmail.com>
2023-05-06 01:35:54 +0800

    Initial set up for Spring Boot project

---
+++ src/main/java/com/taskmanager/taskmanagerapp/SpringBootTaskManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaskManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTaskManagerAppApplication.class, args);
+	}
+
+}
